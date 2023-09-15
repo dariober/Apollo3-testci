b92ab4888a5730a780fed72f4ff49572324e16b1
dariober <dario.beraldi@gmail.com>
2023-09-15 12:27:51 +0100

    Change order of tests

--- packages/jbrowse-plugin-apollo/cypress/e2e/addAssembly.cy.ts
+++ packages/jbrowse-plugin-apollo/cypress/e2e/addAssembly.cy.ts
@@ -17,12 +17,12 @@ describe('Add Assembly', () => {
     cy.wait('@changes').its('response.statusCode').should('match', /2../)
   })
 
-  it('Can add assembly from 2bit', () => {
+  it('Can add assembly from gff3 with fasta', () => {
     cy.contains('Apollo').click()
     cy.contains('Add Assembly').click()
     cy.get('input[type="TextField"]').type('volvox_deleteme')
-    cy.get('input[value="text/x-fasta"]').check()
-    cy.get('input[type="file"]').selectFile('test_data/volvox.2bit')
+    cy.get('input[value="text/x-gff3"]').check()
+    cy.get('input[type="file"]').selectFile('test_data/volvox.fasta.gff3')
 
     cy.intercept('/changes').as('changes')
     cy.contains('Submit').click()
@@ -30,12 +30,12 @@ describe('Add Assembly', () => {
     cy.wait('@changes').its('response.statusCode').should('match', /2../)
   })
 
-  it('Can add assembly from gff3 with fasta', () => {
+  it('Can add assembly from 2bit', () => {
     cy.contains('Apollo').click()
     cy.contains('Add Assembly').click()
     cy.get('input[type="TextField"]').type('volvox_deleteme')
-    cy.get('input[value="text/x-gff3"]').check()
-    cy.get('input[type="file"]').selectFile('test_data/volvox.fasta.gff3')
+    cy.get('input[value="text/x-fasta"]').check()
+    cy.get('input[type="file"]').selectFile('test_data/volvox.2bit')
 
     cy.intercept('/changes').as('changes')
     cy.contains('Submit').click()
